c78d5732052485e58b89267f19e5f86637be5a18
bimon-alta <bimon@alterra.id>
2020-01-22 09:50:32 +0700

    pre CI/CD deployment, deploy.ssh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+eval "$(ssh-agent -s)" &&
+ssh-add -k ~/.ssh/id_rsa &&
+cd /var/www/backend    #direktori pada server (ec2 aws)
+git pull
+
+source ~/.profile
+echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdin
+docker stop gudangworkout2
+docker rm gudangworkout2
+docker rmi bimonalta/gudang_workout:be2
+docker run -d --name gudangworkout2 -p 5000:5000 bimonalta/gudang_workout:latest
